032c3792efd932c46a307a8ed5dbe4143c841ddc
Azuras03 <nr03russo@gmail.com>
2024-02-22 13:25:59 +0100

    Ajout de couleurs personnalisées et ajout de boutons

--- infraWeb/src/components/Navbar.vue
+++ infraWeb/src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <script>
-import  { RouterLink } from 'vue-router'
 </script>
 <template>
-  <nav class="flex flex-col items-center py-4 w-1/6 h-screen bg-gray-100 border-l-2">
-    <RouterLink to="/" class="my-2 font-bold">Home</RouterLink>
-    <RouterLink to="/prout" class="my-2 font-bold">Prout</RouterLink>
+  <nav class="flex flex-col items-center py-4 w-1/6 h-screen bg-green-boite border-l-2 rounded-2xl">
+    <RouterLink to='/' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded drop-shadow-md m-1">
+      Home
+    </RouterLink>
+    <RouterLink to='/prout' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded drop-shadow-md m-1">
+      Prout
+    </RouterLink>
+    <RouterLink to='/about' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded drop-shadow-md m-1">
+      About
+    </RouterLink>
   </nav>
 </template>
