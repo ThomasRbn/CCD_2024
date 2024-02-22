c1b5f2387672d7b84dd005dfec56ab74848085ae
Thomas <robineau1u@etu.univ-lorraine.fr>
2024-02-22 21:46:15 +0100

    [LOGI] fix api url

--- infraLogi/src/CustomHandler.java
+++ infraLogi/src/CustomHandler.java
@@ -43,7 +43,7 @@ public class CustomHandler implements HttpHandler {
         // Vous pouvez par exemple effectuer une requête HTTP vers une autre API
         // Utilisez la classe HttpURLConnection pour cela
 
-        String apiUrl = "https://localhost:8000/api/java/retour";
+        String apiUrl = "localhost:8000/api/java/retour";
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
