838df2e01a79bafe5ffac7ae3fbcb1d85236326e
Thomas <robineau1u@etu.univ-lorraine.fr>
2024-02-22 20:54:44 +0100

    [LOGI] Fix serveur

--- infraLogi/src/CustomHandler.java
+++ infraLogi/src/CustomHandler.java
@@ -43,7 +43,7 @@ public class CustomHandler implements HttpHandler {
         // Vous pouvez par exemple effectuer une requête HTTP vers une autre API
         // Utilisez la classe HttpURLConnection pour cela
 
-        String apiUrl = "https://docketu.iutnc.univ-lorraine.fr:8000/api/optimisation";
+        String apiUrl = "https://docketu.iutnc.univ-lorraine.fr:8000/api/java/retour";
         URL url = new URL(apiUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
