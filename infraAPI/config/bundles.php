4717743a2eeb2d7c4d88b2e26bd8e6c21326a9b6
Thomas <robineau1u@etu.univ-lorraine.fr>
2024-02-25 13:12:46 +0100

    Add API Platform

--- infraAPI/config/bundles.php
+++ infraAPI/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
